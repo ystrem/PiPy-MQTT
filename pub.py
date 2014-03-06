8ca8591afc7e46a0ee60ef9040b5f8ee829bb2c5
ystrem <ystrem@gmail.com>
2014-03-06 22:20:52 +0100

    Added test publish message`s

--- pub.py
+++ pub.py
@@ -1,8 +1,11 @@
 import mosquitto
+from time import sleep
 
 client = mosquitto.Mosquitto("test-client")
 
 client.connect('192.168.1.109', port = 1883)
-client.publish("rbpi", "hello world")
+client.publish("kitchen/light", "on")
+sleep(2)
+client.publish("kitchen/light", "off")
 
 client.disconnect()
