9d8b465509a8ff49ff4f9996c017c05c31c7ed99
Ystrem <ystrem@gmail.com>
2014-03-06 22:19:51 +0100

    Added remote control of GPIO :]

--- broker.py
+++ broker.py
@@ -1,6 +1,14 @@
 #!/usr/bin/python
 
 import mosquitto
+import RPi.GPIO as GPIO
+
+#GPIO.cleanup(17)
+GPIO.setmode(GPIO.BCM)
+GPIO.setup(17, GPIO.OUT)
+
+#Define '#' char
+topic = chr(35)
 
 #define what happens after connection
 def on_connect(rc):
@@ -9,10 +17,13 @@ def on_connect(rc):
 #On recipt of a message create a pynotification and show it
 def on_message(msg):
 	print((msg.topic, msg.payload))
-
+	if msg.payload == 'on':
+		GPIO.output(17, True)
+	elif msg.payload == 'off':
+		GPIO.output(17, False)
 
 #create a broker
-mqttc = mosquitto.Mosquitto("python_sub")
+mqttc = mosquitto.Mosquitto("BROKER-PI")
 
 #define the callbacks
 mqttc.on_message = on_message
@@ -21,8 +32,8 @@ mqttc.on_connect = on_connect
 #connect
 mqttc.connect("192.168.1.109", 1883, 60, True)
 
-#subscribe to topic test
-mqttc.subscribe("rbpi", 2)
+#subscribe to all topic`s
+mqttc.subscribe(topic, 2)
 
 #keep connected to broker
 while mqttc.loop() == 0:
